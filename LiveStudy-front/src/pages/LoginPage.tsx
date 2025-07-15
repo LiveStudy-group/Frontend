e39c2dbcdf01f75ff4c4da550b37db732294da52
JinwonShen <bosv031999@gmail.com>
2025-07-15 16:06:44 +0900

    design: 디자인 가이드에 따른 텍스트 스타일 수정

--- LiveStudy-front/src/pages/LoginPage.tsx
+++ LiveStudy-front/src/pages/LoginPage.tsx
@@ -11,14 +11,14 @@ export default function LoginPage() {
 
         {/* 로그인 버튼들 */}
         <div className="w-full flex flex-col gap-3">
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 hover:bg-primary-400 hover:text-white">Email 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 hover:bg-primary-400 hover:text-white">Google 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 hover:bg-primary-400 hover:text-white">Kakao 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 hover:bg-primary-400 hover:text-white">Naver 로그인</button>
+          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Email 로그인</button>
+          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Google 로그인</button>
+          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Kakao 로그인</button>
+          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl transition-all duration-300 text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Naver 로그인</button>
         </div>
 
         {/* ID/PW 찾기 & 회원가입 */}
-        <div className="w-full flex justify-between text-sm text-gray-500 mt-4">
+        <div className="w-full flex justify-between text-body1_R text-gray-500 mt-4">
           <button>ID/Password 찾기</button>
           <button>
             <Link to={'/join'}>회원가입</Link>
