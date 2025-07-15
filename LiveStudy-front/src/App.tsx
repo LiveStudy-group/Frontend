f70317570551261a8d71eeeef85c7e4657a97103
seojiho <happyluck0426@gmail.com>
2025-07-15 17:30:30 +0900

    feat(StudyRoomPage): 스터디룸페이지 반응형 UI 구현 및 라우터 설정

--- LiveStudy-front/src/App.tsx
+++ LiveStudy-front/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import NotFoundPage from './pages/Error/NotFoundPage';
 import LandingPage from './pages/LandingPage';
+import StudyRoomPage from './pages/StudyRoomPage';
 import TestPage from './pages/TestPage';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='test-page' element={<TestPage />} />
+      <Route path='studyroom/:id' element={<StudyRoomPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Routes>
   )
