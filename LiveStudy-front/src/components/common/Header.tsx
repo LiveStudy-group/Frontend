087dada0a6293ec0c3a8127480448f97973aed8e
seojiho <happyluck0426@gmail.com>
2025-07-15 14:44:21 +0900

    feat(header): 공통 헤더 컴포넌트 구현

--- LiveStudy-front/src/components/common/Header.tsx
+++ LiveStudy-front/src/components/common/Header.tsx
@@ -1,7 +1,12 @@
-export default function Header() {
+const Header = () => {
   return (
-    <header>
-      <h1>My React App</h1>
+    <header className="w-full">
+      <div className="max-w-[1280px] mx-auto px-4 py-4 flex justify-between items-center">
+        <h1 className="text-headline4_B text-primary-600">LiveStudy</h1>
+        <div className="text-caption2_SB text-gray-400">로그인</div>
+      </div>
     </header>
   );
-}
+};
+
+export default Header;
