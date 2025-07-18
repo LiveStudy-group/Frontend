7c36d93111ce92b046c26debaa30df1a483ab823
JinwonShen <bosv031999@gmail.com>
2025-07-19 00:22:19 +0900

    design: login page 스타일 수정 및 email-page ui 구현

--- LiveStudy-front/src/pages/LoginPage.tsx
+++ LiveStudy-front/src/pages/LoginPage.tsx
@@ -2,27 +2,26 @@ import { Link } from "react-router-dom"
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center px-6">
       <section className="w-full max-w-sm flex flex-col items-center">
         {/* 로고 */}
-        <h1 className="text-headline2_B mb-12 text-primary-500">
-          LiveStudy
-        </h1>
+        <div className="mb-12">
+          <h1 className="text-headline2_B mb-2 text-primary-500 text-center">
+            LiveStudy
+          </h1>
+          <p className="text-body1_R text-gray-500 whitespace-nowrap">
+            전국의 모든 친구들과, 집중하는 공부를 시작해보세요.
+          </p>
+        </div>
 
         {/* 로그인 버튼들 */}
         <div className="w-full flex flex-col gap-3">
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Email 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Google 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Kakao 로그인</button>
-          <button className="w-full py-4 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Naver 로그인</button>
-        </div>
-
-        {/* ID/PW 찾기 & 회원가입 */}
-        <div className="w-full flex justify-between text-body1_R text-gray-500 mt-4">
-          <button>ID/Password 찾기</button>
-          <button>
-            <Link to={'/join'}>회원가입</Link>
+          <button className="w-full py-3 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">
+            <Link to={'/email-login'}>Email 로그인</Link>
           </button>
+          <button className="w-full py-3 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Google 로그인</button>
+          <button className="w-full py-3 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Kakao 로그인</button>
+          <button className="w-full py-3 bg-gray-100 border border-gray-300 rounded-xl text-body1_R text-gray-500 hover:bg-primary-400 hover:text-white">Naver 로그인</button>
         </div>
       </section>
     </div>
