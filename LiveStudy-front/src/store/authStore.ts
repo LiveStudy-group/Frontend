93e192f04e93fefc22b662a221e7aeaadddc5dea
JinwonShen <bosv031999@gmail.com>
2025-07-21 23:21:59 +0900

    feat: create 상태 저장소를 만들어 persist적용

--- LiveStudy-front/src/store/authStore.ts
+++ LiveStudy-front/src/store/authStore.ts
@@ -1,15 +1,29 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface AuthState {
   isLoggedIn: boolean;
   username: string | null;
-  login: (username: string) => void;
+  token: string | null;
+
+  login: (username: string, token: string) => void;
   logout: () => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  isLoggedIn: false,
-  username: null,
-  login: (username) => set({ isLoggedIn: true, username }),
-  logout: () => set({ isLoggedIn: false, username: null })
-}))
+export const useAuthStore = create<AuthState>()(
+  persist<AuthState>(
+    (set) => ({
+      isLoggedIn: false,
+      username: null,
+      token: null,
+
+      login: (username, token) => 
+        set({ isLoggedIn: true, username, token }),
+      logout: () => 
+        set({ isLoggedIn: false, username: null, token: null }),
+    }),
+    {
+      name: "auth-storage",
+    }
+  )
+)
