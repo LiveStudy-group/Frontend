7ccc24e497cf60bbbd06ebccc8658f37c15e538d
JinwonShen <bosv031999@gmail.com>
2025-08-05 23:02:39 +0900

    fix: JWT 토큰 인증 헤더 추가로 API 오류 해결

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -13,4 +13,18 @@ const api = axios.create({
   }
 })
 
+// JWT 토큰을 요청 헤더에 자동으로 추가하는 인터셉터
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
