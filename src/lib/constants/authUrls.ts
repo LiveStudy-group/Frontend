c4ae7de8c512aecf3a463f11e460d4aba5016797
seojiho <happyluck0426@gmail.com>
2025-08-08 14:25:11 +0900

    refactor(livekit): FE에서 처리하던 인증 로직을 BE로 이전

--- src/lib/constants/authUrls.ts
+++ src/lib/constants/authUrls.ts
@@ -1,5 +1,30 @@
-export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${import.meta.env.VITE_GOOGLE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_GOOGLE_REDIRECT_URI}&response_type=code&scope=profile email`;
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL!;
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}&response_type=code`;
+export const GOOGLE_AUTH_URL =
+  'https://accounts.google.com/o/oauth2/v2/auth?' +
+  new URLSearchParams({
+    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID!,
+    redirect_uri: `${VITE_API_BASE_URL}/oauth2/authorization/google`,
+    response_type: 'code',
+    scope: 'openid profile email',
+    access_type: 'offline',
+    prompt: 'consent',
+  });
 
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${import.meta.env.VITE_NAVER_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_NAVER_REDIRECT_URI}&state=STATE_STRING`;
+
+export const KAKAO_AUTH_URL =
+  'https://kauth.kakao.com/oauth/authorize?' +
+  new URLSearchParams({
+    client_id: import.meta.env.VITE_KAKAO_CLIENT_ID!,
+    redirect_uri: `${VITE_API_BASE_URL}/oauth2/authorization/kakao`,
+    response_type: 'code',
+  });
+
+export const NAVER_AUTH_URL =
+  'https://nid.naver.com/oauth2.0/authorize?' +
+  new URLSearchParams({
+    client_id: import.meta.env.VITE_NAVER_CLIENT_ID!,
+    redirect_uri: `${VITE_API_BASE_URL}/oauth2/authorization/naver`,
+    response_type: 'code',
+    state: 'liveStudyStateToken',
+  });
